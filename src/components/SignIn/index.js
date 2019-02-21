cc7903f587e044477dfe122736b852a6ca74cd9d
zip520123 <zip520123@yahoo.com.tw>
2019-02-21 18:05:53 +0800

    add signUp and signIn

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,7 +1,57 @@
-import React from 'react';
-const App = () => (
-  <div>
-    <h1>sign in</h1>
-  </div>
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { SignUpLink } from '../SignUp'
+import { withFirebase } from '../Firebase'
+import * as ROUNTES from '../../constants/routes'
+const SignInPage = () => (
+  <>
+    <h1>Sign In</h1>
+    <SignInForm />
+    <SignUpLink />
+  </>
 )
-export default App;
+const INITIAL_STATE = {
+  email:'',
+  password:'',
+  error: null
+}
+class SignInFormBase extends Component {
+  constructor(props){
+    super(props)
+    this.state = { ...INITIAL_STATE }
+  }
+  onSubmit = e => {
+    const {email, password} = this.state
+    console.log(this.state.email, this.state.password)
+    this.props.firebase
+    .doSignInWithEmailAndPassword(email, password)
+    .then(()=>{
+      this.setState({...INITIAL_STATE})
+      this.props.history.push(ROUNTES.HOME)
+    })
+    .catch(error=>{
+      this.setState({error})
+    })
+    e.preventDefault()
+  }
+  onChange = e => {
+    console.log(this.state.email, this.state.password)
+    this.setState({ [e.target.name]:e.target.value})
+  }
+  render(){
+    const { email, password, error } = this.state
+    const isInvalid = password === '' || email ===''
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input name="email" value={email} onChange={this.onChange} type="text" placeholder="Email Address" />
+        <input name="password" value={password} onChange={this.onChange} type="password" placeholder="Password" />
+        <button disabled={isInvalid} type="submit">Sign In</button>
+        {error && <p>{error.message}</p>}
+      </form>
+    )
+  }
+}
+const SignInForm = compose(withRouter,withFirebase)(SignInFormBase)
+export default SignInPage
+export { SignInForm }
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,70 @@
-import React from 'react';
-const App = () => (
-  <div>
-    <h1>sign up</h1>
-  </div>
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import * as ROUNTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
+import { compose } from 'recompose'
+const SignUpPage = () => (
+  <>
+    <h1>SignUp</h1>
+    <SignUpForm></SignUpForm>
+  </>
 )
-export default App;
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+}
+class SignUpFormBase extends Component {
+  constructor(props){
+    super(props)
+    this.state = {...INITIAL_STATE}
+  }
+  onSubmit = e => {
+    const {username, email, passwordOne} = this.state
+    this.props.firebase.doCreateUserWithEmailAndPassword(email, passwordOne).then(auth => {
+      this.setState({ ...INITIAL_STATE})
+      this.props.history.push(ROUNTES.HOME)
+    }).catch(error =>{
+      this.setState({ error })
+    })
+    e.preventDefault();
+  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state
+    const isInvaild = 
+      passwordOne !== passwordTwo || 
+      passwordOne === '' || 
+      email === '' || 
+      username === ''
+    
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input name="username" value={username} onChange={this.onChange} type="text" placeholder="Full Name" />
+        <input name="email" value={email} onChange={this.onChange} type="text" placeholder="email" />
+        <input name="passwordOne" value={passwordOne} onChange={this.onChange} type="password" placeholder="password" />
+        <input name="passwordTwo" value={passwordTwo} onChange={this.onChange} type="password" placeholder="confirm password" />
+        <button disabled={isInvaild} type="submit">Sign Up</button>
+        {error && <p>{error.message}</p>}
+      </form>
+    )
+  }
+}
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUNTES.SIGN_UP}>Sign up</Link>
+  </p>
+)
+const SignUpForm = compose(withRouter,withFirebase,)(SignUpFormBase)
+export default SignUpPage
+export { SignUpForm , SignUpLink}
